dc9021258f47aee5fecd9b70bf9e1545ca03dd7c
Gaurav <gk44035@gmail.com>
2024-04-19 19:13:04 +0530

    routing

--- Frontend/src/router/AllRoutes.jsx
+++ Frontend/src/router/AllRoutes.jsx
@@ -1,20 +1,34 @@
 import React, { useEffect, useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Signin from "../pages/Signin";
 import Signup from "../pages/Signup";
 import Home from "../pages/Home";
 import axios from "axios";
 import { baseUrl } from "../App";
+import { useNavigate } from "react-router-dom";
+import { useAuthContext } from "../context/AuthContext";
 
 const AllRoutes = () => {
+  const { authUser, setAuthUser } = useAuthContext();
+  setAuthUser(localStorage.getItem("user"));
+  // console.log(localStorage.getItem("auth"));
+  console.log(authUser, "authUser");
+  const navigate = useNavigate();
   // const [authenticated, setAuthenticated] = useState(
-  //   JSON.parse(localStorage.getItem("auth"))
+  //   localStorage.getItem("auth")
   // );
   // const authCheck = async (req, res) => {
   //   try {
-  //     const auth = await axios.get(`${baseUrl}/api/authCheck`);
-  //     console.log(auth, "cclll");
+  //     const auth = await axios.get(`${baseUrl}/authCheck`, {
+  //       withCredentials: true,
+  //     });
+  //     console.log(auth.data, "auth");
   //     if (auth.status === 200) {
+  //       // if (localStorage.getItem("auth")) {
+  //       navigate("/home");
+  //       // } else {
+  //       //   navigate("/signin");
+  //       // }
   //       localStorage.setItem("auth", true);
   //     }
   //   } catch (error) {
@@ -23,12 +37,21 @@ const AllRoutes = () => {
   // };
   // useEffect(() => {
   //   authCheck();
-  // }, []);
+  // }, [authenticated]);
   return (
     <Routes>
-      <Route path='/signin' element={<Signin />} />
-      <Route path='/' element={<Signup />} />
-      <Route path='/home' element={<Home />} />
+      <Route
+        path='/signin'
+        element={authUser ? <Navigate to={"/"} /> : <Signin />}
+      />
+      <Route
+        path='/signup'
+        element={authUser ? <Navigate to={"/"} /> : <Signup />}
+      />
+      <Route
+        path='/'
+        element={authUser ? <Home /> : <Navigate to={"/signin"} />}
+      />
     </Routes>
   );
 };
