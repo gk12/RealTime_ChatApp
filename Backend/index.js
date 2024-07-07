5d431671fc0be08d7dca0e922b9a50f3bee44d79
Gaurav <gk44035@gmail.com>
2024-07-08 00:18:03 +0530

    index home

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,8 @@
-const path = require('path');
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const { app, server } = require('./socket/socket');
+const path = require("path");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const { app, server } = require("./socket/socket");
 
 // const app = express();
 app.use(express.json());
@@ -10,41 +10,47 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: 'http://localhost:3000',
+    origin: "http://localhost:3000",
   })
 );
 // run frontend from server
 const dirname = path.resolve();
 
-const database = require('./db/db');
-const dotenv = require('dotenv');
+const database = require("./db/db");
+const dotenv = require("dotenv");
 dotenv.config();
 const port = process.env.PORT || 4006;
-const authRouter = require('./routes/auth.routes');
-const messageRouter = require('./routes/message.routes');
-const userRouter = require('./routes/user.routes');
-const verifyToken = require('./middlewares/verifyToken');
+const authRouter = require("./routes/auth.routes");
+const messageRouter = require("./routes/message.routes");
+const userRouter = require("./routes/user.routes");
+const verifyToken = require("./middlewares/verifyToken");
 
 // authentication route
-app.use('/api/auth', authRouter);
+app.use("/api/auth", authRouter);
 // message route
-app.use('/api/messages', verifyToken, messageRouter);
+app.use("/api/messages", verifyToken, messageRouter);
 // user route
-app.use('/api/users', verifyToken, userRouter);
-app.use('/api/authCheck', verifyToken, async (req, res) => {
+app.use("/api/users", verifyToken, userRouter);
+app.use("/api/authCheck", verifyToken, async (req, res) => {
   try {
     res.status(200).json({
-      message: 'Authorized User',
+      message: "Authorized User",
     });
   } catch (error) {
     res.status(401).json({
-      message: 'Unauthorized User',
+      message: "Unauthorized User",
     });
   }
 });
-app.use(express.static(path.join(dirname, '/Frontend/dist')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(dirname, 'Frontend', 'dist', 'index.html'));
+app.get("/home", (req, res) => {
+  console.log("home");
+  res.json({
+    message: "home",
+  });
+});
+app.use(express.static(path.join(dirname, "/Frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(dirname, "Frontend", "dist", "index.html"));
 });
 function serverConnection() {
   // app.listen(port, () => {
